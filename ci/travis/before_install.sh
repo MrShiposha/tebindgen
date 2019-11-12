623856e0e656df0bcd14baec0ad2cb3a29efc07c
Daniel Shiposha <shipdan@yandex.ru>
2019-11-12 12:59:59 +0300

    Refactor .travis.yml

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod -R +x $HOME/ci/travis
+chmod -R +x $TRAVIS_BUILD_DIR/ci/travis
 
 # Required for running tests (for libclang.so)
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/clang-7.0.0/lib:target/debug/deps
